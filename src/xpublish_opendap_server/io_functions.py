a125ce8c48c39afe7859ff2cae9bb7d7ea817ef9
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-04-26 18:33:41 -0400

    Simplifying IO

--- src/xpublish_opendap_server/io_functions.py
+++ src/xpublish_opendap_server/io_functions.py
@@ -16,36 +16,7 @@ from typing import (
 )
 
 
-def fsspec_open_dataset(
-    file_system: fsspec.AbstractFileSystem,
-    data_path: str,
-    engine: str,
-) -> fsspec.core.OpenFile:
-    # zarr files must be opened as a mapping
-    if engine == 'zarr':
-        return fsspec.mapping.FSMap(
-            data_path,
-            file_system,
-        )
-
-    # all other files can be opened as a file
-    else:
-        return file_system.open(data_path)
-
-
 class CatalogToXarray(abc.ABC):
-
-    @abc.abstractproperty
-    def get_engine_dict(self) -> Dict[str, str]:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_xarray_engine(self) -> str:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_filesystem(self) -> fsspec.AbstractFileSystem:
-        raise NotImplementedError
 
     @abc.abstractmethod
     def write_attributes(
@@ -66,61 +37,20 @@ class IntakeToXarray(CatalogToXarray):
 
     def __init__(
         self,
-        catalog_yaml_path: Path,
+        catalog_yaml_path: Union[Path, str],
     ) -> None:
 
         # create an intake catalog object
         if catalog_yaml_path:
+            catalog_yaml_path = Path(catalog_yaml_path)
             self.catalog: intake.catalog.Catalog = intake.open_catalog(
                 catalog_yaml_path,
             )
-
-    @property
-    def get_engine_dict(self) -> Dict[str, str]:
-        return {
-            'netcdf': 'h5netcdf',
-            'ndzarr': 'zarr',
-            'zarr': 'zarr',
-            'numpy': 'scipy',
-            'rasterio': 'rasterio',
-        }
-
-    def get_xarray_engine(self) -> Union[str, None]:
-        """Get the xarray.open_dataset() engine from the catalog object."""
-
-        # verify the necessary driver plugin is installed
-        driver: str = self.catalog.describe()['driver'][0]
-        if driver not in intake.registry.keys():
+        else:
             raise ValueError(
-                f'{driver} driver not installed. '
-                f'Please install the necessary intake plugin!'
+                'Please provide a valid input to create a catalog object! .'
+                'I.e., a path to a yaml file or a yaml string.'
             )
-        return self.get_engine_dict.get(driver, None)
-
-    def get_filesystem(self) -> fsspec.AbstractFileSystem:
-        """Get the file system from the catalog object."""
-        # get the endpoint type
-        endpoint_key: str = self.catalog.urlpath.split(':')[0]
-        if endpoint_key not in ['s3', 'https']:
-            if endpoint_key in list(string.ascii_uppercase):
-                endpoint_key = 'file'
-            else:
-                raise ValueError(
-                    f'Endpoint type {endpoint_key} not supported. '
-                    f'Please use s3, https, or file.'
-                )
-
-        # get storage options
-        try:
-            storage_options: self.catalog.describe()['args']['storage_options']
-        except KeyError:
-            storage_options = {}
-
-        # start an appropriate filesystem
-        return fsspec.filesystem(
-            endpoint_key,
-            **storage_options,
-        )
 
     def write_attributes(
         self,
@@ -130,7 +60,7 @@ class IntakeToXarray(CatalogToXarray):
 
         info_dict = self.catalog.describe()
         if 'name' in info_dict.keys():
-            ds.name = info_dict['name']
+            ds['name'] = info_dict['name']
         if 'description' in info_dict.keys():
             ds.attrs['description'] = info_dict['description']
         ds.attrs['url_path'] = self.catalog.urlpath
@@ -144,7 +74,7 @@ class IntakeToXarray(CatalogToXarray):
 
         # find the object in the catalog (sub-catalog)
         try:
-            intake_catalog_obj: intake.catalog.Catalog = self.catalog[catalog_item_name]
+            intake_catalog_obj: intake.source.base.DataSource = self.catalog[catalog_item_name]
             info_dict: Dict[str, Any] = intake_catalog_obj.describe()
         except KeyError:
             raise KeyError(f'{catalog_item_name} not found in catalog.')
@@ -156,22 +86,38 @@ class IntakeToXarray(CatalogToXarray):
                 f'Container={info_dict["container"]}'
             )
 
-        # open the file with fsspec
-        file_system = self.get_filesystem()
-        engine = self.get_xarray_engine()
-        open_file = fsspec_open_dataset(
-            file_system=file_system,
-            file_path=info_dict['args']['urlpath'],
-            engine=engine,
-        )
+        # verify the necessary driver plugin is installed
+        driver: str = info_dict['driver'][0]
+        if driver not in intake.registry.keys():
+            raise ValueError(
+                f'{driver} driver not installed. '
+                f'Please install the necessary intake plugin!'
+            )
 
         # open as a xarray dataset and add attributes
-        ds: xr.Dataset = xr.open_dataset(
-            open_file,
-            engine=engine,
-        )
-
+        ds: xr.Dataset = intake_catalog_obj.to_dask()
         ds = self.write_attributes(ds)
 
         # return the dataset
         return ds
+
+
+class STACToXarray(CatalogToXarray):
+
+    def __init__(
+        self,
+        catalog_json_path: Union[Path, str],
+    ) -> None:
+        raise NotImplementedError
+
+    def write_attributes(
+        self,
+        ds: xr.Dataset,
+    ) -> xr.Dataset:
+        raise NotImplementedError
+
+    def get_dataset_from_catalog(
+        self,
+        catalog_item_name: str,
+    ) -> xr.Dataset:
+        raise NotImplementedError
