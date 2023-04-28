7a3c75d074d9717c2d202061a0b1b3b4600817cf
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-04-28 17:01:32 -0400

    Create __init__.py

---
+++ src/xpublish_opendap_server/catalog_search/__init__.py
@@ -0,0 +1,7 @@
+"""Init file for catalog search module."""
+from xpublish_opendap_server.catalog_search.intake_catalog_search import (
+    IntakeCatalogSearch,
+)
+from xpublish_opendap_server.catalog_search.stac_catalog_search import (
+    STACCatalogSearch,
+)
