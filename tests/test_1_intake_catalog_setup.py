f7fa65a7f380c2cc65e321aa3dd878ef3d77aebd
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-06-23 15:33:39 -0400

    Automatically checks if you have AWS credentials in testing

--- tests/test_1_intake_catalog_setup.py
+++ tests/test_1_intake_catalog_setup.py
@@ -1,6 +1,8 @@
 """A pytest module for testing whether Intake catalog setup functions are working properly."""
 import intake
 import pytest
+import boto3
+import warnings
 import xarray as xr
 from pathlib import Path
 from typing import (
@@ -28,6 +30,20 @@ from catalog_to_xpublish.routers import (
 
 
 @pytest.fixture(scope='session')
+def aws_credentials() -> bool:
+    """Whether we have active AWS credentials or not.
+
+    If we do, return True. If not, return False.
+    If False, tests will only be ran on OSN data.
+    """
+    try:
+        boto3.client('s3')
+        return True
+    except Exception:
+        return False
+
+
+@pytest.fixture(scope='session')
 def catalog_path() -> Path:
     """Returns the path to the test catalog."""
     if Path.cwd().name == 'Catalog-To-Xpublish':
@@ -43,6 +59,22 @@ def catalog_path() -> Path:
         'test_intake_zarr_catalog.yaml'
 
 
+def does_requester_pay(
+    io_class: IntakeToXarray,
+    ds_name: str,
+) -> bool:
+    """Check whether an intake dataset is in a requester pays bucket."""
+    storage_options: dict = getattr(
+        io_class.catalog[ds_name],
+        'storage_options',
+        {},
+    )
+    return storage_options.get(
+        'requester_pays',
+        False,
+    )
+
+
 def test_factory() -> None:
     assert isinstance(IntakeCatalogSearch, CatalogSearcherClass)
     assert isinstance(IntakeToXarray, CatalogIOClass)
@@ -53,9 +85,9 @@ def test_factory() -> None:
     assert isinstance(obj, CatalogImplementation)
 
 
-# @pytest.fixture(scope='session')
 def test_catalog_classes(
     catalog_path: Path,
+    aws_credentials: bool,
 ) -> None:
     """Tests parsing of an intake catalog."""
 
@@ -79,17 +111,46 @@ def test_catalog_classes(
 
     # check that the parser finds valid datasets
     for cat_end in catalog_endpoints:
-        if cat_end.contains_datasets:
-            assert len(cat_end.dataset_ids) >= 1
+        if not cat_end.contains_datasets:
+            continue
 
-            for i, ds_name in enumerate(cat_end.dataset_ids):
-                assert isinstance(ds_name, str)
-                assert ds_name in cat_end.dataset_info_dicts.keys()
-                if i == 0:
-                    # check if we can read a dataset
-                    io_class = obj.catalog_to_xarray(
-                        catalog_obj=cat_end.catalog_obj,
-                    )
-                    ds = io_class.get_dataset_from_catalog(dataset_id=ds_name)
-                    assert isinstance(ds, xr.Dataset)
-                    assert ds_name == ds.attrs['name']
+        assert len(cat_end.dataset_ids) >= 1
+
+        for ds_name in cat_end.dataset_ids:
+            assert isinstance(ds_name, str)
+            assert ds_name in cat_end.dataset_info_dicts.keys()
+
+        # check if we can read a dataset
+        tested_ds = False
+        i = 0
+
+        while not tested_ds:
+            if i == len(cat_end.dataset_ids):
+                warnings.warn(
+                    f'Could not find a dataset that can be read for catalog '
+                    f'{cat_end.catalog_obj.name}. Test coverage is not complete.'
+                )
+                break
+            ds_name = cat_end.dataset_ids[i]
+            io_class = obj.catalog_to_xarray(
+                catalog_obj=cat_end.catalog_obj,
+            )
+
+            # skip if we have a requester pays bucket and no credentials
+            requester_pays: bool = does_requester_pay(
+                io_class=io_class,
+                ds_name=ds_name,
+            )
+            if requester_pays and not aws_credentials:
+                warnings.warn(
+                    f'No AWS credentials found (or could not configure). '
+                    f'Tests will only be ran on OSN data!',
+                )
+                i += 1
+                continue
+
+            # check that the dataset is read ok
+            tested_ds = True
+            ds = io_class.get_dataset_from_catalog(dataset_id=ds_name)
+            assert isinstance(ds, xr.Dataset)
+            assert ds_name == ds.attrs['name']
