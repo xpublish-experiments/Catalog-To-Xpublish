ecb37ca94a08d58a3510f4e8bba3f1b793dc0a60
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-11-30 14:15:20 -0500

    If the DatasetProviderPlugin for given catalog endpoint can't be registered -> warn/skip

--- src/catalog_to_xpublish/server_functions.py
+++ src/catalog_to_xpublish/server_functions.py
@@ -156,7 +156,12 @@ def create_app(
                 plugin=provider_plugin,
                 plugin_name=cat_prefix,
             )
-            assert cat_prefix in rest_server.plugins
+            
+            if not cat_prefix in rest_server.plugins:
+                logger.warn(
+                    f'Could not add dataset provider plugin for {cat_prefix} to the server!',
+                )
+                continue
 
             # add all non-dataset provider plugins
             for plugin in app_inputs.xpublish_plugins:
